fcf2184bcb1a45cd51f2b51c1a9c04590729f886
Wang Ru <rockidog@live.com>
2014-09-27 08:01:15 +0800

    Change the packet size

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -4,7 +4,7 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class UDPServer {
-    private final static int MAX_BUFFER_SIZE = 50;
+    private final static int MAX_BUFFER_SIZE = 30;
     public static void main(String[] args) {
         int port = 7001;
         byte[] buffer = new byte[MAX_BUFFER_SIZE];
